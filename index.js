4c87f51f0d98a03907a478c3754fea029266865f
Iroda Yılmaz <irodaand@hotmail.com>
2023-03-20 02:29:04 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(adjective= "*"){
+    return function(adjective2 ="special"){
+        return "You are "+ adjective + adjective2 + adjective + "!";
+    };
+}
